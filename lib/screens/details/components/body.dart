56cf09130adc7d695108dd088573ba9d0c3e1712
unknown <moh.syarif93@gmail.com>
2021-01-21 08:09:06 +0800

    plant app init

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constats.dart';
+import 'package:plant_app/screens/details/components/image_and_icons.dart';
+import 'package:plant_app/screens/details/components/title_and_price.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: FlatButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  color: kPrimaryColor,
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text("Description"),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: kDefaultPadding * 2)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/components/recomend_plants.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constats.dart';
+import 'package:plant_app/screens/details/detail_screen.dart';
+
+class RecomendsPlants extends StatelessWidget {
+  const RecomendsPlants({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          RecommendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Rusia",
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen()),
+              );
+            },
+            price: 440,
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Samantha",
+            country: "Rusia",
+            press: () {},
+            price: 440,
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Rusia",
+            press: () {},
+            price: 440,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendPlantCard extends StatelessWidget {
+  const RecommendPlantCard({
+    Key key,
+    this.image,
+    this.title,
+    this.country,
+    this.price,
+    this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * .4,
+      child: Column(
+        children: [
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(.23),
+                  )
+                ],
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$$price",
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
